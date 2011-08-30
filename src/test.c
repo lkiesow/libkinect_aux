9ba865e7edd281e4041fb79ae770f6113e0b1df6
denmeyer <denmeyer@uni-osnabrueck.de>
2011-08-30 13:00:46 +0200

    Modified the test porgram for better keystrokes.

--- src/test.c
+++ src/test.c
@@ -16,43 +16,80 @@
 #include "kinect_aux.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void * continuous_autolevel( void * ptr ) {
-	
-	while ( 1 ) {
-		kinectaux_autolevel();
-		usleep( 10 );
-	}
-	return NULL;
-
+  while ( 1 ) {
+    kinectaux_autolevel();
+    usleep( 10 );
+  }
+  return NULL; 
 }
 
+/* keyboard hit */
+int kbhit(void) {
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+  
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+  
+  ch = getchar();
+  
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+  
+  if(ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+  
+  return 0;
+}
 
 int main( int argc, char ** argv ) {
-	if( !kinectaux_init( 0 ) ) {
-		return EXIT_FAILURE;
-	}
+  if( !kinectaux_init( 0 ) ) {
+    return EXIT_FAILURE;
+  }
+  
+  pthread_t t;
+  pthread_create( &t, NULL, continuous_autolevel, NULL );
+  
+  printf("Auto leveling\nPress <Enter> to continue...\n");
+  getc( stdin );
+  
+  int ch = 0;
 
-	pthread_t t;
-	pthread_create( &t, NULL, continuous_autolevel, NULL );
+  /*
+  printf("Data\nPress <Esc> to continue...\n");
+  while(ch != 27) {
+    kinectaux_data d = kinectaux_get_data();
+    printf( "%d %u %f\n", d.raw.acc.x, (uint16_t) d.raw.acc.x, d.conv.acc.x );
+    usleep( 10000 );
+    if(kbhit()) {
+      ch=getchar();
+    }
+  }
 
-	getc( stdin );
+  ch = 0;
+  */
 
-	/*
-	int i;
-	for ( i = 0; i < 10000; i++ ) {
-		kinectaux_data d = kinectaux_get_data();
-		printf( "%d %u %f\n", d.raw.acc.x, (uint16_t) d.raw.acc.x, d.conv.acc.x );
-		usleep( 10000 );
-	}
-
-	for ( i = 0; i < 10000; i++ ) {
-		printf( "Your kinect is rotated %f degrees sideways.\n", kinectaux_lateral_tilt() );
-		usleep( 10000 );
-	}
-	*/
-
-	kinectaux_exit();
-	return EXIT_SUCCESS;
+  printf("Rotation\nPress <Esc> to quit...\n");
+  while(ch != 27) {
+    printf( "Your kinect is rotated %f degrees sideways.\n", kinectaux_lateral_tilt() );
+    usleep( 10000 );
+    if(kbhit()) {
+      ch=getchar();
+    }
+  }
+  
+  kinectaux_exit();
+  return EXIT_SUCCESS;
 }
